b428039985c75de397e8f207232fe52b1fc8804e
Proxima-bear <medvedev32134@gmail.com>
2022-10-19 20:23:17 +0500

    getData_commit

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./Card.css";
+import Button from "../Button/Button";
+function Card({ food, onAdd, onRemove }) {
+  const [count, setCount] = useState(0);
+  const { title, Image, price, id } = food;
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+    onAdd(food);
+  };
+  const handleDecrement = () => {
+    setCount(count - 1);
+    onRemove(food);
+  };
+
+  return (
+    <div className="card">
+     
+      <div className="image__container">
+        <img src={Image} alt={title} />
+      </div>
+      <h4 className="card__title">
+        {title}  <span className="card__price">₽ {price}</span>
+        <div className="btn-container">
+        <Button title={"+"} type={"add"} onClick={handleIncrement}   />
+        {count !== 0 ? (
+          <Button title={"-"} type={"remove"} onClick={handleDecrement} />
+        ) : (
+          ""
+        )}
+      </div>
+      </h4>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/Components/db/db.js
@@ -0,0 +1,14 @@
+import forceImg from "../images/force-image.svg"
+export function getData() {
+  return [
+    { title: "", price: 5000, Image: forceImg,id:1 },
+    { title: "", price: 5000, Image: forceImg,id:2 },
+    { title: "", price: 5000, Image: forceImg,id:3 },
+    { title: "", price: 5000, Image: forceImg,id:4 },
+    { title: "", price: 5000, Image: forceImg,id:5 },
+    { title: "", price: 5000, Image: forceImg,id:6 },
+    { title: "", price: 5000, Image: forceImg,id:7 },
+    { title: "", price: 5000, Image: forceImg,id:8 },
+    { title: "", price: 5000, Image: forceImg,id:9 },
+  ];
+}
