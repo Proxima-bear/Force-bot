ebece7272e743decf1e28faeee00ecf6eea5aba0
Proxima-bear <medvedev32134@gmail.com>
2022-10-23 04:27:29 +0500

    utb_commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React,{ useState, useEffect } from "react";
 import "./App.css";
 import Card from "./Components/Card/Card";
 import Cart from "./Components/Cart/Cart";
